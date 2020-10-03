3b8ee5585afbc0d4ab56af3e52eb551084881d90
Pedro Rodriguez <me@pedro.ai>
2020-10-03 03:13:26 -0400

    fix scripts

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,29 +1,29 @@
-sbatch slurm-run.sh glove_bilstm1 1
-sbatch slurm-run.sh glove_bilstm2 2
+sbatch slurm-run.sh glove_bilstm 1
+sbatch slurm-run.sh glove_bilstm 2
 
-sbatch slurm-run.sh glove_bilstm-known1 1
-sbatch slurm-run.sh glove_bilstm-known2 2
+sbatch slurm-run.sh glove_bilstm-known 1
+sbatch slurm-run.sh glove_bilstm-known 2
 
-sbatch slurm-run.sh glove_bilstm-da1 1
-sbatch slurm-run.sh glove_bilstm-da2 2
+sbatch slurm-run.sh glove_bilstm-da 1
+sbatch slurm-run.sh glove_bilstm-da 2
 
-sbatch slurm-run.sh glove_bilstm-like1 1
-sbatch slurm-run.sh glove_bilstm-like2 2
+sbatch slurm-run.sh glove_bilstm-like 1
+sbatch slurm-run.sh glove_bilstm-like 2
 
-sbatch slurm-run.sh glove_bilstm-facts1 1
-sbatch slurm-run.sh glove_bilstm-facts2 2
+sbatch slurm-run.sh glove_bilstm-facts 1
+sbatch slurm-run.sh glove_bilstm-facts 2
 
-sbatch slurm-run.sh bert1 1
-sbatch slurm-run.sh bert2 2
+sbatch slurm-run.sh bert 1
+sbatch slurm-run.sh bert 2
 
-sbatch slurm-run.sh bert-known1 1
-sbatch slurm-run.sh bert-known2 2
+sbatch slurm-run.sh bert-known 1
+sbatch slurm-run.sh bert-known 2
 
-sbatch slurm-run.sh bert-da1 1
-sbatch slurm-run.sh bert-da2 2
+sbatch slurm-run.sh bert-da 1
+sbatch slurm-run.sh bert-da 2
 
-sbatch slurm-run.sh bert-like1 1
-sbatch slurm-run.sh bert-like2 2
+sbatch slurm-run.sh bert-like 1
+sbatch slurm-run.sh bert-like 2
 
-sbatch slurm-run.sh bert-facts1 1
-sbatch slurm-run.sh bert-facts2 2
+sbatch slurm-run.sh bert-facts 1
+sbatch slurm-run.sh bert-facts 2
