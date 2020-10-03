37748b546904aa1555b42c8726683ce6a89f2c64
Pedro Rodriguez <me@pedro.ai>
2020-10-03 03:15:07 -0400

    fix

--- slurm-run.sh
+++ slurm-run.sh
@@ -20,6 +20,6 @@ export SLURM_LOG_FILE="/fs/clip-quiz/entilzha/logs/${SLURM_JOB_ID}.log"
 pwd
 # $1 is the model name, eg "glove_bilstm"
 # $2 is the random seed, eg 42
-srun allennlp train --include-package curiosity -s models/${1}${2} -f configs/generated/${1} -o '{"trainer": {"cuda_device": 0}, "pytorch_seed": '${2}', "numpy_seed": '${2}', "random_seed": '${2}'}'
+srun allennlp train --include-package curiosity -s models/${1}${2} -f configs/generated/${1}.json -o '{"trainer": {"cuda_device": 0}, "pytorch_seed": '${2}', "numpy_seed": '${2}', "random_seed": '${2}'}'
 srun allennlp evaluate --include-package curiosity --output-file experiments/${1}${2}_val_metrics.json models/${1}${2} dialog_data/curiosity_dialogs.val.json
 srun allennlp evaluate --include-package curiosity --output-file experiments/${1}${2}_test_metrics.json models/${1}${2} dialog_data/curiosity_dialogs.test.json
