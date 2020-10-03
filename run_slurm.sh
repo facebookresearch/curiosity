533fbe18cf22a109928c2a307e409e3a884cc86c
Pedro Rodriguez <me@pedro.ai>
2020-10-03 03:09:21 -0400

    add slurm scripts

---
+++ run_slurm.sh
@@ -0,0 +1,29 @@
+sbatch slurm-run.sh glove_bilstm1 1
+sbatch slurm-run.sh glove_bilstm2 2
+
+sbatch slurm-run.sh glove_bilstm-known1 1
+sbatch slurm-run.sh glove_bilstm-known2 2
+
+sbatch slurm-run.sh glove_bilstm-da1 1
+sbatch slurm-run.sh glove_bilstm-da2 2
+
+sbatch slurm-run.sh glove_bilstm-like1 1
+sbatch slurm-run.sh glove_bilstm-like2 2
+
+sbatch slurm-run.sh glove_bilstm-facts1 1
+sbatch slurm-run.sh glove_bilstm-facts2 2
+
+sbatch slurm-run.sh bert1 1
+sbatch slurm-run.sh bert2 2
+
+sbatch slurm-run.sh bert-known1 1
+sbatch slurm-run.sh bert-known2 2
+
+sbatch slurm-run.sh bert-da1 1
+sbatch slurm-run.sh bert-da2 2
+
+sbatch slurm-run.sh bert-like1 1
+sbatch slurm-run.sh bert-like2 2
+
+sbatch slurm-run.sh bert-facts1 1
+sbatch slurm-run.sh bert-facts2 2
---
+++ slurm-run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=curiosity
+#SBATCH --time=0-02:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu-short
+#SBATCH --chdir=/fs/clip-quiz/entilzha/code/curiosity
+#SBATCH --output=/fs/www-users/entilzha/logs/%A.log
+#SBATCH --error=/fs/www-users/entilzha/logs/%A.log
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=10g
+#SBATCH --partition=gpu
+
+set -x
+hostname
+nvidia-smi
+source /fs/clip-quiz/entilzha/anaconda3/etc/profile.d/conda.sh > /dev/null 2> /dev/null
+conda activate curiosity
+export SLURM_LOG_FILE="/fs/clip-quiz/entilzha/logs/${SLURM_JOB_ID}.log"
+pwd
+# $1 is the model name, eg "glove_bilstm"
+# $2 is the random seed, eg 42
+srun allennlp train --include-package curiosity -s models/${1} -f configs/generated/${1} -o '{"trainer": {"cuda_device": 0}, "pytorch_seed": '${2}', "numpy_seed": '${2}', "random_seed": '${2}'}'
+srun allennlp evaluate --include-package curiosity --output-file experiments/${1}_val_metrics.json ${1} dialog_data/curiosity_dialogs.val.json
+srun allennlp evaluate --include-package curiosity --output-file experiments/${1}_test_metrics.json ${1} dialog_data/curiosity_dialogs.test.json
